f51e35300e1a212856323a0f5e25d281fe552427
contarsst <mb_matsiyev@mail.ru>
2023-12-17 17:19:06 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -20,6 +20,7 @@ public class Counter {
     }
 
     public static void main(String[] args) {
+        System.out.println(1);
         System.out.println(sumByEven(1, 8));
         System.out.println(sum(3, 8));
     }
