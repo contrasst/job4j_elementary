a6a9cc4b51b4ed77354fffd43904d3e7c85f6ed4
contarsst <mb_matsiyev@mail.ru>
2024-01-20 20:14:01 +0300

    6.4.3. Слово заканчивается на ... [#156317]

---
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] postfix) {
+        if (word.length < postfix.length) {
+            return false;
+        }
+        for (int i = 0; i < postfix.length; i++) {
+            if (word[word.length - i - 1] != postfix[postfix.length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
