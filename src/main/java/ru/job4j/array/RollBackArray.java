593bf2c63a27e223ea803884cb0644dca208cfa5
contarsst <mb_matsiyev@mail.ru>
2024-01-20 19:48:09 +0300

    6.4.2. Обход массива с последнего элемента [#361619 #475197]

--- src/main/java/ru/job4j/array/RollBackArray.java
+++ src/main/java/ru/job4j/array/RollBackArray.java
@@ -6,6 +6,6 @@ public class RollBackArray {
         for (int i = 0; i < array.length; i++) {
             result[array.length - i - 1] = array[i];
         }
-        return result;
+         return result;
     }
 }
