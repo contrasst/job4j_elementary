b3781ce5b3d8989effd7dc189f20ba5d9cb0a78e
contarsst <mb_matsiyev@mail.ru>
2024-01-26 21:06:43 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049]

---
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+public class SimpleStringEncoder {
+    public static String encode(String input) {
+        String result = "";
+        char symbol = input.charAt(0);
+        int counter = 1;
+
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == symbol) {
+                counter++;
+            } else {
+                if (counter == 1) {
+                    result = result + symbol;
+                    symbol = input.charAt(i);
+                    counter = 1;
+                } else {
+                    result = result + symbol + counter;
+                    symbol = input.charAt(i);
+                    counter = 1;
+                }
+            }
+        }
+        return counter <= 1 ? result + symbol : result + symbol + counter;
+    }
+    }
